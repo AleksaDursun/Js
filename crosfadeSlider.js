a0df530e58e772e75b849065f406ed650c0d1d14
Aleksa Dursun <45974884+AleksaDursun@users.noreply.github.com>
2019-03-20 23:13:32 +0100

    Just changed the comments

--- crosfadeSlider.js
+++ crosfadeSlider.js
@@ -1,30 +1,29 @@
 var slideIndex = 0;
 carousel();
-/* Function whitch controls image change */
+/* Function whitch controls image change automatically bz fading from one to another */
 function carousel() {
     var i;
     var x = document.getElementsByClassName("slide");
-    for (i = 0; i < x.length; i++) {
-      
+    for (i = 0; i < x.length; i++) 
+    {      
       x[i].style.display = "none";
       if(slideIndex!=0)
       {
         if(i==slideIndex-1)
         {
-          x[i].style.display = "block";
-          x[i].style.zIndex = "1"; 
+          x[i].style.display = "block";//keeps the previous image shown
+          x[i].style.zIndex = "1"; //puts it infront of all images except the next one
         }
       }
       else
       {
-        x[x.length-1].style.zIndex = "1";
+        x[x.length-1].style.zIndex = "1";//if slide index is 0 it shows the last image in the carousel
       }
-      
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    x[slideIndex-1].style.zIndex = "2";
-    x[slideIndex-1].style.animation="fade 1000ms linear 0s 1 ";
-    setTimeout(carousel, 5000);
+    if (slideIndex > x.length) {slideIndex = 1}//reverses the slide index
+    x[slideIndex-1].style.display = "block";//shows the next image 
+    x[slideIndex-1].style.zIndex = "2";//puts it in front 
+    x[slideIndex-1].style.animation="fade 1000ms linear 0s 1 ";//activates the animation
+    setTimeout(carousel, 5000);//rince and repeat
 }
